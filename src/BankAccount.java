366f8ec7f889ad5aa758df033e97d2c5b7333b5e
Quinnathin <quinnboyer475@gmail.com>
2025-02-05 20:43:16 +0000

    BankAccountDone!

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,6 @@
-
+public class BankAccount{
+    public static void main(String args[]){
+        double bobsBankAccount = (((((((2175.40 - 302.50) + 50)/2) + 20) - 1) * 2) + 1);
+        System.out.println(String.format("Bob's current balance is %.2f", bobsBankAccount));
+    }
+}
